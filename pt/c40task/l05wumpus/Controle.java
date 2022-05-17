bdfbd629162eb828e2187604ab4f6f0aba7ad950
luucoelho <luizacoelho2@gmail.com>
2022-05-17 12:45:12 -0300

    primeira implementação controle

--- pt/c40task/l05wumpus/Controle.java
+++ pt/c40task/l05wumpus/Controle.java
@@ -1,5 +1,11 @@
 package pt.c40task.l05wumpus;
 
 public class Controle {
-    
+    private String name;
+    private int score;
+    private char status;
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
