4eee0272d4db76ae7e45ab9ea0f34b57804f5cda
luucoelho <luizacoelho2@gmail.com>
2022-05-17 14:04:21 -0300

    implementar setters e funcao modelo de movimento

--- pt/c40task/l05wumpus/Controle.java
+++ pt/c40task/l05wumpus/Controle.java
@@ -4,8 +4,29 @@ public class Controle {
     private String name;
     private int score;
     private char status;
+    private Heroi heroi;
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public void getScore(int score) {
+        this.score = score;
+    }
+
+    public void getStatus(char status) {
+        this.status = status;
+    }
+
+    public void setHeroi(Heroi heroi) {
+        this.heroi = heroi;
+    }
+
+    public int moveUp(){
+        if (this.heroi.getCoord().getLin() != 1){
+            Coordenada dest = new Coordenada(this.heroi.getCoord().getLin() - 1, this.heroi.getCoord().getCol());
+            this.heroi.mv(dest);
+        }
+        
     }
 }
