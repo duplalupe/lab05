2c7af42420b1c2ee52145e2cba8f334b317ef880
luucoelho <luizacoelho2@gmail.com>
2022-05-17 10:50:59 -0300

    aplica algumas verificações

--- pt/c40task/l05wumpus/Montador.java
+++ pt/c40task/l05wumpus/Montador.java
@@ -5,6 +5,10 @@ import java.util.Scanner;
 public class Montador {
     private Caverna cave;
     private String[][] caveStrings;
+    private int numBuracos = 0;
+    private int numOuros = 0;
+    private int numHerois = 0;
+    private int numWumpus = 0;
     
     public Montador(String[][] caveStrings){
         this.caveStrings = caveStrings;
@@ -24,13 +28,26 @@ public class Montador {
 
             switch(caveStrings[i][2]){
                 case "P":
+                if (numHerois < 1) {
                     comp = new Heroi();
+                    numHerois++;
+                }
                 case "B":
+                if (numBuracos < 3) {
                     comp = new Buraco(coord);
+                    numBuracos++;
+                }
+                // pensando em como verificar se tem menos de 2 buracos
                 case "O":
+                if (numOuros < 1) {
                     comp = new Ouro(coord);
+                    numOuros++;
+                }
                 case "W":
+                if (numWumpus < 1) {
                     comp = new Wumpus(coord);
+                    numWumpus++;
+                }
                 default:
                     comp = null;
             }
