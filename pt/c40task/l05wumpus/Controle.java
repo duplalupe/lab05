93b08e07dc59f7609fe29533fd55f83a0a7fd3fc
luucoelho <luizacoelho2@gmail.com>
2022-05-18 19:40:11 -0300

    primeiras condições de pontuação

--- pt/c40task/l05wumpus/Controle.java
+++ pt/c40task/l05wumpus/Controle.java
@@ -5,6 +5,7 @@ public class Controle {
     private int score;
     private char status;
     private Heroi heroi;
+    private Sala sala;
 
     public void setName(String name) {
         this.name = name;
@@ -22,11 +23,59 @@ public class Controle {
         this.heroi = heroi;
     }
 
+    public int ajustePontuacao() {
+        score += 15;
+        if (this.heroi.captGold()) {
+            score += 1000;
+        }
+        if (this.sala.isWumpus()) {
+            score -= 1000;
+        }
+        if (this.sala.isBuraco()) {
+            score -= 1000;
+        }
+        if (this.heroi.killWumpus()) {
+            score += 500;
+        }
+        return score;
+    }
+
     public int moveUp(){
         if (this.heroi.getCoord().getLin() != 1){
             Coordenada dest = new Coordenada(this.heroi.getCoord().getLin() - 1, this.heroi.getCoord().getCol());
             this.heroi.mv(dest);
         }
-        
+        score = ajustePontuacao();
+        return score;
+        //checa o que tem na sala dest
+    }
+
+    public int moveDown(){
+        if (this.heroi.getCoord().getLin() != 4){
+            Coordenada dest = new Coordenada(this.heroi.getCoord().getLin() + 1, this.heroi.getCoord().getCol());
+            this.heroi.mv(dest);
+        }
+        //checa o que tem na sala dest
+        score = ajustePontuacao();
+        return score;
+    }
+
+    public int moveLeft(){
+        if (this.heroi.getCoord().getCol() != 1){
+            Coordenada dest = new Coordenada(this.heroi.getCoord().getLin(), this.heroi.getCoord().getCol() - 1);
+            this.heroi.mv(dest);
+        }
+        //checa o que tem na sala dest
+        score = ajustePontuacao();
+        return score;
+    }
+    public int moveRight(){
+        if (this.heroi.getCoord().getCol() != 4){
+            Coordenada dest = new Coordenada(this.heroi.getCoord().getLin(), this.heroi.getCoord().getCol() + 1);
+            this.heroi.mv(dest);
+        }
+        //checa o que tem na sala dest
+        score = ajustePontuacao();
+        return score;
     }
 }
